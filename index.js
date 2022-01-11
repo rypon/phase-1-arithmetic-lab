bef2ec5678a2d57a50e5bb2f8be7ace5ff9031e7
Ryan O'Neill <ryanponeill00@gmail.com>
2022-01-10 20:46:09 -0500

    commit tree b7ebaa6ad0dcfa0973535b1e7b0bf6e1b95eac20

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10)
+console.log(random)
+
+
+let num3 = 8
+let num4 = 2
+const mod = num3 / num4
+
+const max = Math.max(1, 2, 4, 8, 16, 20)
